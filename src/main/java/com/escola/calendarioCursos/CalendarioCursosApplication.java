59afc300b4bb18464ea9979645034f3af3ce2a62
Klayton Ramires <ramires03@hotmail.com>
2023-07-25 11:00:18 -0300

    first commit

---
+++ src/main/java/com/escola/calendarioCursos/CalendarioCursosApplication.java
@@ -0,0 +1,13 @@
+package com.escola.calendarioCursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarioCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarioCursosApplication.class, args);
+	}
+
+}
